84451ceded9ee2539def0eb80b0624fb9b0a594a
Ryan Hafner <r.hafner20@gmail.com>
2021-04-22 12:50:41 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,245 @@
+const clamp = (val, min, max) => Math.min(Math.max(min, val), max);
+
+const hexToRgb = hex => {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+class UtilityCanvas {
+    constructor({ canvas = document.createElement('canvas'), width = 1024, height = 1024, parent = null } = {}) {
+        this.canvas = canvas;
+        this.width = canvas.width = width;
+        this.height = canvas.height = height;
+        this.ctx = canvas.ctx = canvas.getContext('2d');
+        if (parent) parent.appendChild(canvas);
+    }
+
+    resize({ width = this.width, height = this.height }) {
+        this.width = this.canvas.width = width;
+        this.height = this.canvas.height = height;
+        return this;
+    }
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        return this;
+    }
+
+    // Settings
+    setColor(color = null) {
+        if (color) this.ctx.fillStyle = color;
+        return this;
+    }
+    // add cap style, dashed lines, etc.
+    setStroke({ thickness = null, color = null } = {}) {
+        if (thickness) this.ctx.lineWidth = thickness;
+        if (color) this.ctx.strokeStyle = color;
+        return this;
+    }
+
+    // Fills
+    fill(color = null) {
+        this.setColor(color);
+        this.ctx.fill();
+        return this;
+    }
+    fillColor(color) {
+        this.ctx.save();
+        this.setColor(color);
+        this.ctx.fillRect(0, 0, this.width, this.height);
+        this.ctx.restore();
+        return this;
+    }
+    fillTexture(image, preserveAspect = false) {
+        if (preserveAspect) return this.fillOrFitTextureWithAspect({ image, fill: preserveAspect === 'fill' });
+        else this.ctx.drawImage(image, 0, 0, this.width, this.height);
+        return this;
+    }
+    fillOrFitTextureWithAspect({ 
+        image, fill = true, aspect, 
+        anchors = { x: 'center', y: 'center' }, 
+        offset = { x: 0, y: 0 },
+        margin = { t: 0, l: 0, r: 0, b: 0 }
+    }) {
+        aspect = aspect || (image.naturalHeight || image.height) / (image.naturalWidth || image.width);
+        if (typeof margin === 'number') margin = { t: margin, l: margin, r: margin, b: margin }
+        const { t, l, r, b } = margin;
+        const maxWidth = this.width - l - r;
+        const maxHeight = this.height - t - b;
+
+        const comparison = aspect / (maxHeight / maxWidth);
+        if (comparison === 1) this.ctx.drawImage(image, offset.x + l, offset.y + t, maxWidth, maxHeight);
+        else if ((comparison > 1) === fill) {
+            // constrain height because image is taller (fit) or wider (fill)
+            const height = maxWidth * aspect;
+            const yOffset = anchors.y === 'bottom' ? maxHeight - height: 
+                anchors.y === 'top' ? 0: (maxHeight - height) / 2;
+            this.ctx.drawImage(image, offset.x + l, yOffset + offset.y + t, maxWidth, height);
+        }
+        else {
+            // constrain width because image is wider (fit) or taller (fill)
+            const width = maxHeight / aspect;
+            const xOffset = anchors.x === 'right' ? maxWidth - width: 
+                anchors.x === 'left' ? 0: (maxWidth - width) / 2;
+            this.ctx.drawImage(image, xOffset + offset.x + l, offset.y + t, width, maxHeight);
+        }
+        if (t || l || r || b) {
+            this.ctx.save();
+            this.ctx.globalCompositeOperation = 'destination-in';
+            this.ctx.fillStyle = '#ffffff';
+            this.ctx.fillRect(t, l, maxWidth, maxHeight);
+            this.ctx.restore();
+        }
+        return this;
+    }
+    fillBackground({ color = null, image = null, alpha = this.ctx.globalAlpha } = {}) {
+        this.ctx.save();
+        this.ctx.globalCompositeOperation = 'destination-over';
+        this.ctx.globalAlpha = alpha;
+        if (color) this.fillColor(color);
+        else if (image) this.ctx.drawImage(image, 0, 0, this.width, this.height);
+        this.ctx.restore();
+        return this;
+    }
+
+    // Stroke
+    beginPath() {
+        this.ctx.beginPath();
+        return this;
+    }
+    stroke(strokeOptions = {}) {
+        this.setStroke(strokeOptions);
+        this.ctx.stroke();
+        return this;
+    }
+    closePath() {
+        this.ctx.closePath();
+        return this;
+    }
+
+    // Shapes
+    rectangle({ position: { x = 0, y = 0 }, width = this.width, height = this.height }) {
+        this.ctx.rect(x, y, width, height);
+        return this;
+    }
+    arc({ center: { x, y }, radius, startAngle = 0, endAngle = 2 * Math.PI, cc = false }) {
+        this.ctx.arc(x, y, radius, startAngle, endAngle, cc);
+        return this;
+    }
+    ellipse({ center: { x, y }, radius: { x: rx, y: ry }, rotation = 0, startAngle = 0, endAngle = 2 * Math.PI, cc = false }) {
+        this.ctx.ellipse(x, y, rx, ry, rotation, startAngle, endAngle, cc);
+        return this;
+    }
+    roundedRectangle({ position: { x = 0, y = 0 }, width, height, radius }) {
+        if (typeof radius === 'number') radius = { tl: radius, tr: radius, bl: radius, br: radius }
+        const { tl, tr, bl, br } = radius;
+        
+        this.ctx.beginPath();
+        this.ctx.moveTo(x + tl, y);
+        this.ctx.lineTo(x + width - tr, y);
+        // TR
+        this.ctx.arc(x + width - tr, y + tr, tr, Math.PI / 2, 0);
+        this.ctx.lineTo(x + width, y + height - br);
+        // BR
+        this.ctx.arc(x + width - br, y + height - br, br, 0, Math.PI / 2);
+        this.ctx.lineTo(x + bl, y + height);
+        // BL
+        this.ctx.arc(x + bl, y + height - bl, bl, -Math.PI / 2, -Math.PI);
+        this.ctx.lineTo(x, y + tl);
+        // TL
+        this.ctx.arc(x + tl, y + tl, tl, Math.PI, Math.PI / 2);
+        this.ctx.closePath();
+        return this;
+    }
+
+    // Textures
+    draw({ image, position: { x, y } = {}, rotation = 0, scale = 1 }) {
+        this.ctx.save();
+        this.ctx.translate(x, y);
+        this.ctx.rotate(rotation);
+        this.ctx.drawImage(
+            image, 
+            -image.width * scale / 2, 
+            -image.height * scale / 2, 
+            image.width * scale, 
+            image.height * scale
+        );
+        this.ctx.restore();
+        return this;
+    }
+    intersect(image) {
+        this.ctx.save();
+        this.ctx.globalCompositeOperation = 'destination-in';
+        this.ctx.drawImage(image, 0, 0, this.width, this.height);
+        this.ctx.restore();
+        return this;
+    }
+    intersectWithRotation(image, rotation) {
+        this.ctx.save();
+        this.ctx.globalCompositeOperation = 'destination-in';
+        this.draw(image, { x: 0.5, y: 0.5 }, rotation, this.width / (1.414 * image.width));
+        this.ctx.restore();
+        return this;
+    }
+    removeTexture({ image, position: { x, y }, rotation = 0, scale = 1 }) {
+        this.ctx.save();
+        this.ctx.translate(x, y);
+        this.ctx.rotate(rotation);
+        this.ctx.globalCompositeOperation = 'destination-out';
+        this.ctx.drawImage(
+            image,
+            -image.width * scale / 2,
+            -image.height * scale / 2,
+            image.width * scale,
+            image.height * scale
+        );
+        this.ctx.restore();
+        return this;        
+    }
+
+    chromaKey(color, threshold = 0.01) {
+        const rgb = (typeof color === 'string') ? hexToRgb(color): color;
+        if (!rgb) {
+            console.error(`Invalid color value: ${color}`);
+            return this;
+        }
+        threshold = 3 * 255 * 255 * Math.pow(clamp(threshold, 0.01, 1), 2);
+        const imageData = this.ctx.getImageData(0, 0, this.width, this.height);
+        let distSq;
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            distSq = Math.pow(imageData.data[i] - rgb.r, 2) + Math.pow(imageData.data[i + 1] - rgb.g, 2) + Math.pow(imageData.data[i + 2] - rgb.b, 2);
+            if (distSq <= threshold) imageData.data[i + 3] = 0;
+        }
+        this.ctx.putImageData(imageData, 0, 0);
+        return this;
+    }
+
+    // Import/Export
+    download(format = 'image/jpeg', name = 'export.jpg') {
+        const link = document.createElement('a');
+        link.download = name;
+        link.href = this.getDataURL(format);
+        link.click();
+    }
+    getDataURL(format = 'image/jpeg', quality = 1) {
+        return this.canvas.toDataURL(format, quality);
+    }
+    setDataURL(data) {
+        const self = this;
+        const image = new Image();
+        return new Promise(resolve => {
+            image.onload = () => {
+                self.fillTexture(image);
+                resolve();
+            }
+            image.src = data;
+        });
+    }
+}
+
+export { UtilityCanvas, clamp, hexToRgb }
+export default UtilityCanvas
