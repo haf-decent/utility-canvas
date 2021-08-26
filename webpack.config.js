8a1d60c90d6fc6e6a94b2cdc60b28607570cfd88
Ryan Hafner <r.hafner20@gmail.com>
2021-08-26 14:21:33 -0400

    create tsc and webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,21 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './src/index.js',
+    entry: './src/index.ts',
     output: {
+        path: path.resolve( __dirname, 'dist' ),
         filename: 'utility-canvas.min.js',
-        path: path.resolve(__dirname, 'dist/')
     },
-    watch: true
-}
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    }
+};
