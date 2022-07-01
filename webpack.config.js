4b6b810432af0aa62d25cd0e9ca000ac3e6229bc
Ryan Hafner <r.hafner20@gmail.com>
2022-06-30 23:50:33 -0400

    update typescript to support GlobalCompositeOperation type, formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve( __dirname, 'dist' ),
-        filename: 'utility-canvas.min.js',
-    },
-    resolve: {
-        extensions: [ '.ts', '.js' ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    }
+	mode: 'production',
+	entry: './src/index.ts',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'utility-canvas.min.js',
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			}
+		]
+	}
 };
